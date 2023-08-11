1adf4bd8af40ac78336f6fa3659f6e4a3b063f66
adhilcodes <abduladhilpk@gmail.com>
2023-08-11 16:04:53 +0530

    added featurs

--- awesomecalculator-main/src/calculator/core.py
+++ awesomecalculator-main/src/calculator/core.py
@@ -1,9 +1,12 @@
+import math
+
 class Calculator:
     def __init__(self):
         pass
 
-    def Add(self, value1, value2):
-        return value1 + value2
+    # This function can add unlimited any of number
+    def Add_(self, *values):
+        return sum(values)
 
     def Substract(self, value1, value2):
         return value1 - value2
@@ -13,3 +16,14 @@ class Calculator:
 
     def Divide(self, value1, value2):
         return value1 / value2
+        
+    # This function will calculate the square-root of the given number and return the result
+    def Square_root(self, num):
+        return math.sqrt(num)
+        
+    # This function will calculate the percentage of the given value and return the result
+    def Percentage(value, percentage):
+    	return (value*percentage)/100
+    	
+ 
+
